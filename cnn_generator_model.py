7a01cc901211214d249a5c6a3ef19bad99e6f054
Yuval Kittenplon <yuval.kittenplon@live.biu.ac.il>
2021-08-16 17:25:05 +0300

    update

--- cnn_generator_model.py
+++ cnn_generator_model.py
@@ -146,7 +146,7 @@ def main():
                                        save_weights_only=True,
                                        mode='max')
 
-    fit_log = model.fit(train_flow, validation_data=val_flow, epochs=50,
+    fit_log = model.fit(train_flow, validation_data=val_flow, epochs=100,
                         callbacks=[model_checkpoint])
 
     model.evaluate(test_flow, verbose=1)
