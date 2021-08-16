c88b9028a082ca74b9bf298c4c93db3179c8d5a8
Yuval Kittenplon <yuval.kittenplon@live.biu.ac.il>
2021-08-16 10:58:18 +0300

    update

--- cnn_model.py
+++ cnn_model.py
@@ -9,7 +9,10 @@ from matplotlib import pyplot as plt
 import time
 
 from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
+def get_available_devices():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos]
+print(get_available_devices())
 
 
 dataset_path = '/home/access/yuval_projects/data/Animals-10'
