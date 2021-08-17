9e64b6206a8124f0e7fea8e10af46d39200dbed2
Yuval Kittenplon <yuval.kittenplon@live.biu.ac.il>
2021-08-17 10:40:39 +0300

    update

--- transfer_model.py
+++ transfer_model.py
@@ -146,9 +146,9 @@ def train_and_evaluate_model(keras_application, keras_application_name):
 def main():
     # train_and_evaluate_model(VGG16, 'VGG16')
     # train_and_evaluate_model(VGG19, 'VGG19')
-    train_and_evaluate_model(ResNet50, 'ResNet50')
+    # train_and_evaluate_model(ResNet50, 'ResNet50')
     # train_and_evaluate_model(ResNet101, 'ResNet101')
-    # train_and_evaluate_model(ResNet152, 'ResNet152')
+    train_and_evaluate_model(ResNet152, 'ResNet152')
 
 
 if __name__ == '__main__':
