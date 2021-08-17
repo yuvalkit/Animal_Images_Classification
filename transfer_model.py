11dcc76aefb67548ce1d4a2626f6e8eb371af5da
Yuval Kittenplon <yuval.kittenplon@live.biu.ac.il>
2021-08-17 14:27:46 +0300

    update

--- transfer_model.py
+++ transfer_model.py
@@ -142,7 +142,7 @@ def train_and_evaluate_model(keras_application, keras_application_name):
 
 
 def main():
-    train_and_evaluate_model(ResNet50, 'ResNet50')
+    train_and_evaluate_model(ResNet101, 'ResNet101')
 
 
 if __name__ == '__main__':
