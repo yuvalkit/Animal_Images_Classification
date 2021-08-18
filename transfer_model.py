8f3b0cc1c46e589de4dc4da2c760ff0cdcfc1cc0
Yuval Kittenplon <yuval.kittenplon@live.biu.ac.il>
2021-08-18 12:43:33 +0300

    update

--- transfer_model.py
+++ transfer_model.py
@@ -160,7 +160,7 @@ def visualize_model(model):
     images = [activation.astype(np.float32) for activation in activations]
     for i in range(0, len(images)):
         visualization = images[i]
-        plt.imshow((visualization * 255).astype(np.uint8), cmap='gray')
+        plt.imshow(visualization.astype(np.uint8), cmap='gray')
         plt.title(categories[i])
         plt.savefig(f'visualizations/{categories[i]}.png')
 
